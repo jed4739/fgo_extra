cb49cdf6babbd8ab4da661960a7df284f80053a2
jed4739 <tjsdlr04@gmail.com>
2022-12-30 13:59:12 +0900

    Default Value

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,35 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title : Text(
+            'Hello Flutter',
+            style: TextStyle(fontSize: 28),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Image.network(""),
+              TextField(
+                decoration: InputDecoration(labelText: '이메일'),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: '비밀번호'),
+                obscureText: true,
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(top: 20),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('로그인'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
