a45753cb59b45a943d22d4b8672cc658a12da037
jed4739 <njsi04@naver.com>
2022-12-31 22:11:58 +0900

    style edit

--- lib/main.dart
+++ lib/main.dart
@@ -15,33 +15,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title : Text(
             'Hello Flutter',
             style: TextStyle(fontSize: 28),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Image.network(""),
-              TextField(
-                decoration: InputDecoration(labelText: '이메일'),
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: '비밀번호'),
-                obscureText: true,
-              ),
-              Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(top: 20),
-                child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text('로그인'),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Image.network(
+                    "https://ih1.redbubble.net/image.620660335.9751/aps,504x498,small,transparent-pad,600x600,f8f8f8.u1.jpg",
+                    width: 300,
+                  ),
                 ),
-              ),
-            ],
+                TextField(
+                  decoration: InputDecoration(labelText: '이메일'),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: '비밀번호'),
+                  obscureText: true,
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.only(top: 20),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('로그인'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
